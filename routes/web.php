329ec3b18b5b90d6e48777d0656ed1a9f707ca5e
dlwngh1113 <dlwngh1113@naver.com>
2022-12-25 16:22:40 +0900

    controller class

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TaskController2;
+use App\Http\Controllers\TaskControllerApi;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TaskController::class, 'index']);
+Route::resource('tasks', TaskController2::class);
+Route::apiResource('apiTask', TaskControllerApi::class);
