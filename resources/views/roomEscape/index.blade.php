908355fd1dbb12a4db5d2d34bc490cb73f413a57
dlwngh1113 <dlwngh1113@naver.com>
2023-01-02 21:27:13 +0900

    html split view

--- resources/views/roomEscape/index.blade.php
+++ resources/views/roomEscape/index.blade.php
@@ -1,29 +1,55 @@
 <!DOCTYPE html>
 <html>
     <head>
-
+    <style>
+        div.row{
+            width:100%;
+        }
+        div.left_column{
+            width:70%;
+            float: left;
+            border: 1ch;
+        }
+        div.right_column{
+            width:30%;
+            float: right;
+            border:1ch;
+        }
+    </style>
     </head>
     <body>
-        <table cellpadding='5' style="align-content: center; border: 1ch">
-            <tr>
-                <th> id </th>
-                <th> name </th>
-                <th> playable_user_cnt </th>
-                <th> play_time </th>
-            </tr>
-            @foreach ($themes as $theme)
-            <form method="post" action="/roomEscape">
-                @csrf
-                <tr>
-                    <td> <input type="submit" name="id" value={{ $theme->id }}> </td>
-                    <td> {{ $theme->name }} </td>
-                    <td> {{ $theme->playable_user_cnt }} </td>
-                    <td> {{ $theme->play_time }} min </td>
-                </tr>
-              </form>
-            @endforeach
-        </table>
+        <div id="container" role="main">
+            <div id="contents" class="left_column">
+                <table cellpadding='5' style="align-content: center; border: 1ch">
+                    <tr>
+                        <th> id </th>
+                        <th> name </th>
+                        <th> playable_user_cnt </th>
+                        <th> play_time </th>
+                    </tr>
+                    @foreach ($themes as $theme)
+                    <form method="post" action="/roomEscape">
+                        @csrf
+                        <tr>
+                            <td> <input type="submit" name="id" value={{ $theme->id }}> </td>
+                            <td> {{ $theme->name }} </td>
+                            <td> {{ $theme->playable_user_cnt }} </td>
+                            <td> {{ $theme->play_time }} min </td>
+                        </tr>
+                    </form>
+                    @endforeach
+                </table>
 
-        {{ $themes->links() }}
+                <div class="row"> {{ $themes->links() }}</div>
+            </div>
+            <div id="login" class="right_column">
+                <form method="POST" action="/roomEscape">
+                <input type="text" value="id">
+                <input type="password" value="password">
+                <button type="submit"> Login</button>
+                </form>
+            </div>
+        </div>
+
     </body>
 </html>
