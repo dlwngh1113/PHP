f50a8ad9f979e5c4c6124923fce2f45ac000d24a
dlwngh1113 <dlwngh1113@naver.com>
2023-01-03 23:49:25 +0900

    user login form make

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,6 +1,28 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <style>
+            div.main{
+                width:50%;
+                text-align: center;
+                align-content: center;
+            }
+            div.banner{
+                width:100%;
+                text-align: center;
+                align-content: center;
+                font-size: 300%;
+            }
+        </style>
+    </head>
     <body>
-                
+        <div class="banner"> This is Banner</div>
+        <div class="main">
+            <form method="POST" action="/user">
+                <input type="text" id="ID" aria-valuetext="ID">
+                <input type="password" id="password" aria-valuetext="password">
+                <button type="submit">Login</button>
+            </form>
+        </div>
     </body>
 </html>
