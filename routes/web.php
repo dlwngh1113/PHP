cf596e77d87d93839601bdfc0bb58bf4527a46d0
dlwngh1113 <dlwngh1113@naver.com>
2023-01-24 21:00:09 +0900

    index redirect to jetstream login

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoomEscapeController;
 use App\Http\Controllers\UserController;
 
-Route::get('/', function() {
-    return view('dashboard');
+Route::get('/', function(){
+    return redirect('roomEscape');
 })->name('verification.notice');
 
 Route::resource('roomEscape', RoomEscapeController::class);
 Route::prefix('user')->name('user.')->group(function(){
-    Route::get('/', [UserController::class, 'index'])->name('login');
+    Route::get('/', function(){
+        return view('dashboard');
+    })->name('login');
     Route::post('/', [UserController::class, 'authenticate']);
     Route::get('/create',[UserController::class, 'create'])->name('create');
 });
