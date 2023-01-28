3dc590e6898822573e21a26b40a2999d7da9af24
dlwngh1113 <dlwngh1113@naver.com>
2023-01-28 23:51:16 +0900

    modified: customized login view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,20 +12,22 @@
     <form method="POST" action="{{ route('validate_login') }}">
         @csrf
         <div>
-            <input type="text" name="email" placeholder="Email">
+            <p class="input-control" style="text-align: left">Email</p>
+            <input class="input-control" type="text" name="email">
             @if ($errors->has('email'))
                 <span class="text-danger">{{ $errors->first('email') }}</span>
             @endif
         </div>
         <div>
-            <input type="password" name="password" placeholder="Password">
+            <p class="input-control" style="text-align: left">Password</p>
+            <input class="input-control" type="password" name="password">
             @if ($errors->has('password'))
                 <span class="text-danger">{{ $errors->first('password') }}</span>
             @endif
         </div>
-        <div>
+        <div style="text-align: right; padding-top: 20px;">
             <a href="{{ route('reset_password') }}">forgot your password?</a>
-            <button type="submit">Login</button>
+            <button class="input-control" type="submit">Login</button>
         </div>
     </form>
 </div>
