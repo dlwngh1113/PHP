0f52266c3ceca40dff4fbe535321264b9c9ca51f
dlwngh1113 <dlwngh1113@naver.com>
2023-01-28 18:32:52 +0900

    add stylesheet to blade file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <ul class="navbar-nav">
-        @guest
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('register') }}">Register</a>
-            </li>
-        @else
-            <li class="nav-item">
-                <a class="nav-item" href="{{ route('logout') }}">Logout</a>
-            </li>
-        @endguest
-        </ul>
+        <div class="header">
+            <ul class="navbar-nav">
+            @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">Register</a>
+                </li>
+            @else
+                <li class="nav-item">
+                    <a class="nav-item" href="{{ route('logout') }}">Logout</a>
+                </li>
+            @endguest
+            </ul>
+        </div>
 
     <div class="container mt-5">
         @yield('content')
