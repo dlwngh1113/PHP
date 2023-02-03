38a4bb017aea257a2bc981c1d8736181379dddfe
LeeJuho <55501976+dlwngh1113@users.noreply.github.com>
2023-02-03 10:43:25 +0900

    refactor: authenticate methods move to middleware

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,15 @@ Route::prefix('freeboard')->name('freeboard.')->middleware(['auth'])->group(func
     Route::get('/{post}', [FreeBoardController::class, 'show'])->name('show');
 });
 
+Route::prefix('profile')->name('profile.')->middleware(['auth'])->group(function(){
+    Route::get('dashboard', [LoginController::class, 'dashboard'])->name('dashboard');
+    Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+    Route::get('reset_password', [LoginController::class, 'reset_password'])->name('reset_password');
+});
+
 Route::controller(LoginController::class)->group(function(){
     Route::get('login', 'login')->name('login');
     Route::get('register', 'register')->name('register');
-    Route::get('logout', 'logout')->name('logout');
     Route::post('register', 'validate_register')->name('validate_register');
     Route::post('login', 'validate_login')->name('validate_login');
-    Route::get('dashboard', 'dashboard')->name('dashboard');
-    Route::get('reset_password', 'reset_password')->name('reset_password');
 });
