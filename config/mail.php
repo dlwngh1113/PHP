187d52af7d16c254ae8911f8b0e4f28a7b60c6a6
LeeJuho <55501976+dlwngh1113@users.noreply.github.com>
2023-02-10 14:03:46 +0900

    feat: default mail driver set to mailgun

--- config/mail.php
+++ config/mail.php
@@ -14,7 +14,7 @@ return [
     */
 
     //'default' => env('MAIL_MAILER', 'smtp'),
-    'default' => 'mailgun',
+    'default' => env('MAIL_MAILER', 'mailgun'),
 
     /*
     |--------------------------------------------------------------------------
