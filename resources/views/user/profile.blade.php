ed95f5460c122a49b7dfe639384697755d0c6ad1
dlwngh1113 <dlwngh1113@naver.com>
2023-02-14 21:25:17 +0900

    feat: container style changed

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -2,31 +2,31 @@
 
 @section('content')
     <div class="container-table">
-        <div class="container-row">
+        <div class="container-leftrow">
             <p>User Name</p>
         </div>
-        <div class="container-row">
+        <div class="container-rightrow">
             <p>{{ $user->name }}</p>
         </div>
 
-        <div class="container-row">
+        <div class="container-leftrow">
             <p>Email</p>
         </div>
-        <div class="container-row">
+        <div class="container-rightrow">
             <p>{{ $user->email }}</p>
         </div>
 
-        <div class="container-row">
+        <div class="container-leftrow">
             <p>Reset Password</p>
         </div>
-        <div class="container-row">
+        <div class="container-rightrow">
             <p>{{ $user->password }}</p>
         </div>
 
-        <div class="container-row">
+        <div class="container-leftrow">
             <p>Delete User</p>
         </div>
-        <div class="container-row">
+        <div class="container-rightrow">
             <p>{{ $user->created_at }}</p>
         </div>
     </div>
