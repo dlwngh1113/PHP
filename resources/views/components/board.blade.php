6a0c3d25abe5af71dbe70f5d37cd96199431d73d
dlwngh1113 <dlwngh1113@naver.com>
2023-02-18 23:17:42 +0900

    fix: redirect to correct page

--- resources/views/components/board.blade.php
+++ resources/views/components/board.blade.php
@@ -1,7 +1,7 @@
 <ul class="banner-label">
     @foreach ($boards as $board)
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('board.index', $board->id) }}">{{ $board->name }}</a>
+            <a class="nav-link" href="{{ route('board.', $board->id) }}">{{ $board->name }}</a>
         </li>
     @endforeach
 </ul>
