d4a2eaca1dd1aae2c82a5ffe25fba19413152263
dlwngh1113 <dlwngh1113@naver.com>
2023-02-18 16:07:04 +0900

    style: change new post view

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,17 +1,19 @@
-@extends('layouts.master')
+@extends('welcome')
 
-@section('content')
+@section('main')
 
+<div class="container">
     <form method="POST" action="{{ route('post.store') }}">
         @csrf
-        <input type="text" name="title" placeholder="title">
+        <p style="min-width: 100%;"><input type="text" name="title" placeholder="title" style="min-width: inherit"></p>
         @error('title')
             <p class="error">{{ $errors->first('title') }}</p>
         @enderror
-        <input type="text" name="content" placeholder="content" size="10vh">
+        <p style="min-width: 100%"><input type="text" name="content" placeholder="content" style="min-height: 50vh; min-width: inherit;"></p>
         @error('content')
             <p class="error">{{ $errors->first('content') }}</p>
         @enderror
         <button type="submit">Put up</button>
     </form>
+</div>
 @endsection
