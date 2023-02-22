ff9b7ea79e64f88e90640a40dca6b848f5128d67
dlwngh1113 <dlwngh1113@naver.com>
2023-02-22 21:18:03 +0900

    refactor: move style to component

--- resources/views/components/popup.blade.php
+++ resources/views/components/popup.blade.php
@@ -1,4 +1,48 @@
-<div class="background show">
+<style>
+    .background{
+        position: fixed;
+        top: 0px;
+        left: 0px;
+        width: 100%;
+        height: 100vh;
+        background-color: rgba(0, 0, 0, 0.3);
+        z-index: 1000;
+
+        z-index: -1;
+        opacity: 0;
+    }
+    .window{
+        position: relative;
+        width: 100%;
+        height: 100%;
+    }
+    .popup{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: #ffffff;
+        box-shadow: 0 2px 7px rgba(0, 0, 0, 0.3);
+
+        width: 500px;
+        height: 500px;
+    }
+    .show{
+        opacity: 1;
+        z-index: 1000;
+        transition: all 0.5s;
+    }
+    .error{
+        color: red;
+        text-align: center;
+    }
+    .info{
+        color: black;
+        text-align: center;
+    }
+</style>
+
+<div class="background">
     <div class="window">
         <div class="popup">
             <div style="text-align: right;">
