5990fabed2c17846d6d43689ccd2e4e60f6014d5
LeeJuho <55501976+dlwngh1113@users.noreply.github.com>
2023-02-27 15:24:21 +0900

    feat: add email verification resend method

--- app/Http/Controllers/VerificationController.php
+++ app/Http/Controllers/VerificationController.php
@@ -15,36 +15,19 @@ class VerificationController extends Controller
         return view('verification.notice');
     }
 
-    // function verify($token = null)
-    // {
-    //     if($token == null) {
-    // 		session()->flash('message', 'Invalid Login attempt');
-    // 	    return redirect()->route('login');
-    //     }
-
-    //     $user = User::where('email_verification_token',$token)->first();
-
-    //     if($user == null )
-    //     {
-    //        session()->flash('message', 'Invalid Login attempt');
-    //        return redirect()->route('login');
-    //     }
-
-    //     $user->update([
-    //         'email_verified' => true,
-    //         'email_verified_at' => Carbon::now()->getTimestamp(),
-    //         'email_verification_token' => ''
-    //     ]);
-
-    //     session()->flash('message', 'Your account is activated, you can log in now');
-    //     return redirect()->route('login');
-    // }
-
     function verify(EmailVerificationRequest $request)
     {
         $request->fulfill();
 
-        session()->flash('message', 'Your account is activated, you can log in now');
+        $request->session()->flash('message', 'Your account is activated, you can log in now');
         return redirect()->route('home');
+    }
+    
+    function resend(Request $request)
+    {
+        $request->user()->sendEmailVerificationNotification();
+        
+        $request->session()->flash('message', 'Verification link sent!');
+        return back();
     }
 }
